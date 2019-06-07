b17594a236eaf45beb117f9ac3527fb4c59d10ee
Elena Pavlova <lenlho22@gmail.com>
2019-06-07 17:37:47 +0300

    Added preview link from urlPrefix & slug

--- src/Main.jsx
+++ src/Main.jsx
@@ -10,12 +10,35 @@ import './style.sass'
 }))
 export default class Main extends Component {
   static propTypes = {
-    fieldValue: PropTypes.bool.isRequired,
+    // fieldValue: PropTypes.bool.isRequired,
+    plugin: PropTypes.object.isRequired,
   }
 
   render() {
-    const { fieldValue } = this.props
+    // const { fieldValue,  plugin } = this.props
+    const { plugin } = this.props
+    const slug = plugin.getFieldValue('slug')
 
-    return <div className="container">{JSON.stringify(fieldValue)}</div>
+    const { urlPrefix } = plugin.parameters.instance
+    console.log('Main plugin', plugin)
+    console.log(plugin.site.attributes.name)
+    console.log(plugin.itemType.attributes.api_key)
+    console.log('=====')
+    console.log(plugin.itemId)
+    console.log(plugin.field.attributes.api_key)
+    console.log(plugin.currentUser.attributes.email)
+    console.log(plugin.parameters.instance.urlPrefix)
+
+    console.log(plugin.getFieldValue(plugin.fieldPath))
+    console.log(plugin.getFieldValue('slug'))
+
+    return (
+      <div className="container">
+        <a href={`${urlPrefix}${slug}`} title={slug} target="_blank" rel="noopener noreferrer" className="preview-link">
+          Preview link
+        </a>
+        <p>{`${urlPrefix}${slug}`}</p>
+      </div>
+    )
   }
 }
